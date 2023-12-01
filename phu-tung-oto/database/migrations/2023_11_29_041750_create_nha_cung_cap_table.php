4c76ae085d72942f4db0678c2d3571bad0cf3744
LuckyPhuc <bmtck0000@gmail.com>
2023-12-01 21:48:34 +0700

    rebuild

---
+++ phu-tung-oto/database/migrations/2023_11_29_041750_create_nha_cung_cap_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nha_cung_cap', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ten_nha_cung_cap');
+            $table->string('dia_chi')->nullable();
+            $table->string('dien_thoai')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nha_cung_cap');
+    }
+};
