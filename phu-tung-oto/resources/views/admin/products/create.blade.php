7985c88220c5f62c27024baf33c159f89c57d0e2
LuckyPhuc <130754340+LuckyPhuc@users.noreply.github.com>
2023-12-04 19:59:53 +0700

    MVC

--- phu-tung-oto/resources/views/admin/products/create.blade.php
+++ phu-tung-oto/resources/views/admin/products/create.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.admin')
 @section('title', 'Admin - Products - add')
 <div class="page-wrapper">
-@section('content')
-<div class="content">
-                <div class="page-header">
-                    <div class="page-title">
-                        <h4>Add Products </h4>
-                        <h6>Create New Products</h6>
-                    </div>
+    @section('content')
+        <div class="content">
+            <div class="page-header">
+                <div class="page-title">
+                    <h4>Add Products </h4>
+                    <h6>Create New Products</h6>
                 </div>
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Products name</label>
-                                    <input type="text">
-                                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Products name</label>
+                                <input type="text">
                             </div>
-                            {{-- <div class="col-lg-3 col-sm-6 col-12">
+                        </div>
+                        {{-- <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="form-group">
                                     <label>Type</label>
                                     <select class="select">
@@ -27,7 +27,7 @@
                                     </select>
                                 </div>
                             </div> --}}
-                            {{-- <div class="col-lg-3 col-sm-6 col-12">
+                        {{-- <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="form-group">
                                     <label>Sub categories</label>
                                     <select class="select">
@@ -36,7 +36,7 @@
                                     </select>
                                 </div>
                             </div> --}}
-                            {{-- <div class="col-lg-3 col-sm-6 col-12">
+                        {{-- <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="form-group">
                                     <label>Brand</label>
                                     <select class="select">
@@ -45,98 +45,98 @@
                                     </select>
                                 </div>
                             </div> --}}
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>PLU</label>
-                                    <input type="text">
-                                </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>PLU</label>
+                                <input type="text">
                             </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Quantity</label>
-                                    <input type="text">
-                                </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Quantity</label>
+                                <input type="text">
                             </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Price</label>
-                                    <input type="text">
-                                </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Price</label>
+                                <input type="text">
                             </div>
-                            <div class="col-lg-12">
+                        </div>
+                        <div class="col-lg-12">
                             <div class="form-group">
                                 <label>Description</label>
                                 <textarea class="form-control"></textarea>
                             </div>
-                            </div>
-                           <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Products images 1 </label>
-                                    <div class="image-upload">
-                                        <input type="file">
-                                        <div class="image-uploads">
-                                            <img src="{{asset('assets/img/icons/upload.svg')}}" alt="img">
-                                            <h4>Drag and drop files to upload</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Product images 2 </label>
-                                    <div class="image-upload">
-                                        <input type="file">
-                                        <div class="image-uploads">
-                                            <img src="{{asset('assets/img/icons/upload.svg')}}" alt="img">
-                                            <h4>Drag and drop files to upload</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Product images 3</label>
-                                    <div class="image-upload">
-                                        <input type="file">
-                                        <div class="image-uploads">
-                                            <img src="{{asset('assets/img/icons/upload.svg')}}" alt="img">
-                                            <h4>Drag and drop files to upload</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Product images 4 </label>
-                                    <div class="image-upload">
-                                        <input type="file">
-                                        <div class="image-uploads">
-                                            <img src="{{asset('assets/img/icons/upload.svg')}}" alt="img">
-                                            <h4>Drag and drop files to upload</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class=" col-12">
-                                <div class="form-group">
-                                    <label>product images 5</label>
-                                    <div class="image-upload">
-                                        <input type="file">
-                                        <div class="image-uploads">
-                                            <img src="{{asset('assets/img/icons/upload.svg')}}" alt="img">
-                                            <h4>Drag and drop files to upload</h4>
-                                        </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Products images 1 </label>
+                                <div class="image-upload">
+                                    <input type="file">
+                                    <div class="image-uploads">
+                                        <img src="{{ asset('assets/img/icons/upload.svg') }}" alt="img">
+                                        <h4>Drag and drop files to upload</h4>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-12 whit">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Product images 2 </label>
+                                <div class="image-upload">
+                                    <input type="file">
+                                    <div class="image-uploads">
+                                        <img src="{{ asset('assets/img/icons/upload.svg') }}" alt="img">
+                                        <h4>Drag and drop files to upload</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Product images 3</label>
+                                <div class="image-upload">
+                                    <input type="file">
+                                    <div class="image-uploads">
+                                        <img src="{{ asset('assets/img/icons/upload.svg') }}" alt="img">
+                                        <h4>Drag and drop files to upload</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Product images 4 </label>
+                                <div class="image-upload">
+                                    <input type="file">
+                                    <div class="image-uploads">
+                                        <img src="{{ asset('assets/img/icons/upload.svg') }}" alt="img">
+                                        <h4>Drag and drop files to upload</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-12">
+                            <div class="form-group">
+                                <label>product images 5</label>
+                                <div class="image-upload">
+                                    <input type="file">
+                                    <div class="image-uploads">
+                                        <img src="{{ asset('assets/img/icons/upload.svg') }}" alt="img">
+                                        <h4>Drag and drop files to upload</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 whit">
                         <a href="#" class="btn btn-submit me-2">Submit</a>
                         <a href="#" class="btn btn-cancel">Cancel</a>
-                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-@endsection
         </div>
+    </div>
+@endsection
+</div>
