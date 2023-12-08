3d88352de1656a6dc4521c4229d3e0650389220a
racingboi <truongduc2004ok@gmail.com>
2023-12-08 22:11:50 +0700

    a

--- phu-tung-oto/resources/views/users/shop.blade.php
+++ phu-tung-oto/resources/views/users/shop.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.app')
 @section('title', 'trang shop')
 @section('content')
-    <div class="banner-fullwidth-area mb-text">
-        <div class="container custom-area">
+    <div class="breadcrumbs-area position-relative">
+        <div class="container">
             <div class="row">
-                <div class=" text-center justify-content-center col-custom">
-                    <div class="banner-flash-content d-flex flex-column align-items-center justify-content-center h-100 ">
-                        <h2 class="deal-head text-uppercase">Shop</h2>
-                        <h3 class="deal-title text-uppercase">
-                            Hurry up and Get 25% Discount
-                        </h3>
-                        {{-- <a href="shop.html" class="obrien-button primary-btn">Shop Now</a> --}}
-                        <div class="countdown-wrapper d-flex justify-content-center" data-countdown="2022/12/24"></div>
+                <div class="col-12 text-center">
+                    <div class="breadcrumb-content position-relative section-content">
+                        <h3 class="title-3 text-white">shop</h3>
+                        <ul>
+                            <li><a href="{{ route('users.index') }}" class="text-white">Home</a></li>
+                            <li class="text-white">products shop</li>
+                        </ul>
                     </div>
                 </div>
-                {{-- <div class="col-md-5 col-lg-6 text-center col-custom">
-                    <div class="banner-thumb h-100 d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('assets/img/product/product69.jpg') }}" alt="Banner Thumb" />
-                    </div>
-                </div> --}}
             </div>
         </div>
     </div>
@@ -300,21 +294,4 @@
     </div>
     <!-- Shop Main Area End Here -->
     <!-- Support Area Start Here -->
-    <div class="support-area">
-        <div class="container container-default custom-area">
-            <div class="row">
-                <div class="col-lg-12 col-custom">
-                    <div class="support-wrapper d-flex">
-                        <div class="support-content">
-                            <h1 class="title">Need Help ?</h1>
-                            <p class="desc-content">Call our support 24/7 at 01234-567-890</p>
-                        </div>
-                        <div class="support-button d-flex align-items-center">
-                            <a class="obrien-button primary-btn" href="contact-us.html">Contact now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
