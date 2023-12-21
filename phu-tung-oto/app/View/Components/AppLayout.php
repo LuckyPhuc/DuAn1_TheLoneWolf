bc8467fc95026c5bb9becff39b28c7fde0042f64
racingboi <truongduc2004ok@gmail.com>
2023-12-21 12:00:29 +0700

    sua loi

--- phu-tung-oto/app/View/Components/AppLayout.php
+++ phu-tung-oto/app/View/Components/AppLayout.php
@@ -8,6 +8,8 @@ use App\Models\Suppliers;
 use App\Models\Categories;
 use App\Models\Posts;
 use App\Models\Products;
+use App\Models\Order_details;
+use App\Models\Orders;
 
 class AppLayout extends Component
 {
@@ -20,6 +22,16 @@ class AppLayout extends Component
         $categories = Categories::all();
         $posts = Posts::all();
         $products = Products::all();
-        return view('profile', compact('suppliers', 'categories', 'products', 'posts'));
+        $user_id = auth()->id();
+        $cart = Order_details::whereHas('order', function ($query) use ($user_id) {
+            $query->where('users_id', $user_id);
+        })
+            ->with(['order.order_details', 'product.image_features'])
+            ->get();
+
+        $groupedCart = $cart->groupBy('product.id');
+        $order_detail = Order_details::all();
+        $order = Orders::all();
+        return view('profile', compact('suppliers', 'categories', 'products', 'posts', 'groupedCart', 'order_detail', 'order'));
     }
 }
